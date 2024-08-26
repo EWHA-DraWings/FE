302b95f6686f3d524a70ba80bd87d01138a0087a
Seo yeon <mariajang16@ewhain.net>
2024-08-27 04:41:13 +0900

    main_screen, diary_screen, pallete 수정 및 라우팅

--- sodam/lib/pallete.dart
+++ sodam/lib/pallete.dart
@@ -10,4 +10,13 @@ class Pallete {
   static const sodamGray = Color.fromARGB(217, 219, 219, 219);
   static const sodamLightPink = Color.fromARGB(255, 206, 138, 178);
   static const sodamDarkGreen = Color.fromARGB(255, 56, 81, 56);
+  //add: main 화면 색상
+  static const sodamIvory = Color(0xFFFCF6EA);
+  static const sodamOrange = Color(0xFFF4705D);
+  static const sodamNewGreen = Color(0xFF5BB287);
+  static const sodamYellow = Color(0xFFECBE5B);
+  static const sodamNewDarkPink = Color(0xFFC0597E);
+  //diary 추가 색상
+  static const sodamBlueGreen = Color(0xFF50A99A);
+  static const sodamLightYellow = Color(0xFFFCDA93);
 }
