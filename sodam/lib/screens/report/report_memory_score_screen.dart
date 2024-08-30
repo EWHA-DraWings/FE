ec594f5f548d01f135bb2864ea35bf5cf27b5dcc
lrenais <bleuing@ewhain.net>
2024-08-30 19:19:11 +0900

    기억 테스트 결과 화면 바 차트 배치(0830)

--- sodam/lib/screens/report/report_memory_score_screen.dart
+++ sodam/lib/screens/report/report_memory_score_screen.dart
@@ -1,6 +1,12 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:sodam/models/memory_score_data.dart';
 import 'package:sodam/pallete.dart';
+import 'package:sodam/widgets/shadow_white_container.dart';
 import 'package:sodam/widgets/title_widget.dart';
 
 class ReportMemoryScoreScreen extends StatelessWidget {
@@ -9,12 +15,71 @@ class ReportMemoryScoreScreen extends StatelessWidget {
   ReportMemoryScoreScreen({super.key, required this.today});
 
   final List<MemoryScoreData> memoryScores = [
-    MemoryScoreData(date: '0812', score: 79),
-    MemoryScoreData(date: '0815', score: 84),
-    MemoryScoreData(date: '0818', score: 67),
-    MemoryScoreData(date: '0821', score: 73),
-    MemoryScoreData(date: '0824', score: 92),
+    MemoryScoreData(date: '8/12', score: 79),
+    MemoryScoreData(date: '8/15', score: 84),
+    MemoryScoreData(date: '8/18', score: 67),
+    MemoryScoreData(date: '8/21', score: 73),
+    MemoryScoreData(date: '8/24', score: 92),
   ];
+
+  //x축 레이블 반환하는 함수
+  Widget getTitles(double value, TitleMeta meta) {
+    const style = TextStyle(
+      color: Colors.black,
+      fontSize: 20,
+      fontFamily: 'IBMPlexSansKRRegular',
+    );
+    String text;
+    switch (value.toInt()) {
+      case 0:
+        text = memoryScores[0].date;
+        break;
+      case 1:
+        text = memoryScores[1].date;
+        break;
+      case 2:
+        text = memoryScores[2].date;
+        break;
+      case 3:
+        text = memoryScores[3].date;
+        break;
+      case 4:
+        text = memoryScores[4].date;
+        break;
+      default:
+        text = '';
+        break;
+    }
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 4,
+      child: Text(text, style: style),
+    );
+  }
+
+  List<BarChartGroupData> buildBarChartGroupDatas() {
+    List<BarChartGroupData> barGroups = [];
+
+    for (int i = 0; i < memoryScores.length; i++) {
+      barGroups.add(
+        BarChartGroupData(
+          x: i,
+          barRods: [
+            BarChartRodData(
+              toY: memoryScores[i].score,
+              width: 20,
+              borderRadius: BorderRadius.circular(5),
+              color: i == memoryScores.length - 1
+                  ? Pallete.sodamOrange
+                  : Pallete.sodamNewGreen,
+            ),
+          ],
+          showingTooltipIndicators: [0],
+        ),
+      );
+    }
+    return barGroups;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,14 +99,98 @@ class ReportMemoryScoreScreen extends StatelessWidget {
           padding: EdgeInsets.symmetric(
             horizontal: screenWidth * 0.05,
           ),
-          child: const Center(
+          child: Center(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                TitleWidget(
+                const TitleWidget(
                   backgroundColor: Pallete.sodamNewDarkPink,
                   textColor: Colors.white,
                   text: '기억테스트 점수 기록',
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: screenWidth * 0.9,
+                  height: screenHeight * 0.4,
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 5,
+                    vertical: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color(0x3F000000),
+                        blurRadius: 4,
+                        offset: Offset(0, 4),
+                        spreadRadius: 0,
+                      ),
+                    ],
+                  ),
+                  child: BarChart(
+                    BarChartData(
+                      maxY: 100,
+                      minY: 10,
+                      //바 선택했을 때
+                      barTouchData: BarTouchData(
+                        enabled: false,
+                        touchTooltipData: BarTouchTooltipData(
+                          getTooltipColor: (group) => Colors.transparent,
+                          tooltipMargin: -5,
+                          tooltipPadding: EdgeInsets.zero,
+                          getTooltipItem: (
+                            BarChartGroupData group,
+                            int groupIndex,
+                            BarChartRodData rod,
+                            int rodIndex,
+                          ) {
+                            return BarTooltipItem(
+                              rod.toY.round().toString(),
+                              const TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                                fontFamily: 'IBMPlexSansKRRegular',
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      //레이블
+                      titlesData: FlTitlesData(
+                        show: true,
+                        bottomTitles: AxisTitles(
+                          sideTitles: SideTitles(
+                            showTitles: true,
+                            reservedSize: 30,
+                            getTitlesWidget: getTitles,
+                          ),
+                        ),
+                        topTitles: const AxisTitles(
+                          sideTitles: SideTitles(showTitles: false),
+                        ),
+                        rightTitles: const AxisTitles(
+                          sideTitles: SideTitles(showTitles: false),
+                        ),
+                        leftTitles: const AxisTitles(
+                          sideTitles: SideTitles(showTitles: false),
+                        ),
+                      ),
+                      borderData: FlBorderData(
+                        show: true,
+                        border: const Border(
+                          top: BorderSide.none,
+                        ),
+                      ),
+                      gridData: const FlGridData(
+                        show: false,
+                      ),
+                      //바 데이터
+                      barGroups: buildBarChartGroupDatas(),
+                    ),
+                  ),
                 ),
               ],
             ),
