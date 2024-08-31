9c426befccfe4f4d8bf63d35615e12382838baad
Seo yeon <mariajang16@ewhain.net>
2024-08-31 20:09:02 +0900

    main_screen animation 수정

--- sodam/lib/widgets/main_page_button.dart
+++ sodam/lib/widgets/main_page_button.dart
@@ -4,7 +4,7 @@ import 'package:sodam/pallete.dart';
 class MainPageButton extends StatefulWidget {
   final Widget destination; // destination : 넘어갈 다음 화면
   final String text;
-  final Color backColor; //버튼 배경색
+  final Color backColor; // 버튼 배경색
   final String iconPath; // 아이콘 이미지의 경로
   final bool isGuardian; // 버튼 활성화 상태를 제어하는 매개변수
 
@@ -21,8 +21,58 @@ class MainPageButton extends StatefulWidget {
   State<MainPageButton> createState() => _MainPageButtonState();
 }
 
-class _MainPageButtonState extends State<MainPageButton> {
-  bool isHovered = false; //클릭 애니메이션
+class _MainPageButtonState extends State<MainPageButton>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<Color?> _colorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 100),
+      vsync: this,
+    );
+
+    // Define the colorTween to lower the saturation of the background color
+    _colorAnimation = ColorTween(
+      begin: widget.backColor,
+      end: _desaturateColor(widget.backColor), // Desaturate the original color
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  // Function to desaturate the color by reducing its saturation
+  Color _desaturateColor(Color color) {
+    final hsl = HSLColor.fromColor(color);
+    final desaturatedHsl =
+        hsl.withSaturation(hsl.saturation * 0.8); // Reduce saturation
+    return desaturatedHsl.toColor();
+  }
+
+  void _onTap() {
+    if (!widget.isGuardian || (widget.text != "대화하기" && widget.text != "일기장")) {
+      _controller.forward().then((_) {
+        _controller.reverse().then((_) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => widget.destination, // 다음 화면
+            ),
+          );
+        });
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // Get the screen width
@@ -33,72 +83,47 @@ class _MainPageButtonState extends State<MainPageButton> {
     return Column(
       children: [
         GestureDetector(
-          onTap: !isDisabled
-              ? () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => widget.destination), // 다음 화면
-                  );
-                }
-              : null, // 비활성화 상태일 때 클릭할 수 없게 설정
-          onTapDown: (_) {
-            setState(() {
-              isHovered = true;
-            });
-          },
-          onTapUp: (_) {
-            setState(() {
-              isHovered = false;
-            });
-          },
-          onTapCancel: () {
-            setState(() {
-              isHovered = false;
-            });
-          },
-          child: AnimatedScale(
-            scale: isHovered ? 1.02 : 1.0,
-            duration: const Duration(milliseconds: 50),
-            curve: Curves.easeInOut,
-            child: Container(
-              width: screenWidth * 0.38,
-              height: screenWidth * 0.38,
-              decoration: BoxDecoration(
-                color: !isDisabled
-                    ? widget.backColor
-                    : Colors.grey.withOpacity(0.5), // 버튼 배경색
-                borderRadius: BorderRadius.circular(30),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.6), // 그림자 색상 및 투명도 설정
-                    spreadRadius: 3, // 그림자 확산 정도
-                    blurRadius: 7, // 그림자 흐림 정도
-                  ),
-                ],
-              ),
-              child: Center(
-                child: Image.asset(
-                  widget.iconPath,
-                  width: 70,
-                  color: !isDisabled ? null : Colors.grey, // 비활성화 시 아이콘 색상 변경
+          onTap: _onTap,
+          onTapDown: (_) => _controller.forward(),
+          onTapUp: (_) => _controller.reverse(),
+          onTapCancel: () => _controller.reverse(),
+          child: AnimatedBuilder(
+            animation: _colorAnimation,
+            builder: (context, child) {
+              return Container(
+                width: screenWidth * 0.38,
+                height: screenWidth * 0.38,
+                decoration: BoxDecoration(
+                  color: isDisabled
+                      ? Colors.grey.withOpacity(0.5)
+                      : _colorAnimation.value,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.8), // 그림자 색상 및 투명도 설정
+                      spreadRadius: 2, // 그림자 확산 정도
+                      blurRadius: 5, // 그림자 흐림 정도
+                    ),
+                  ],
                 ),
-              ),
-            ),
+                child: Center(
+                  child: Image.asset(
+                    widget.iconPath,
+                    width: 70,
+                    color: isDisabled ? Colors.grey : null, // 비활성화 시 아이콘 색상 변경
+                  ),
+                ),
+              );
+            },
           ),
         ),
         const SizedBox(height: 10), // 아이콘과 텍스트 사이에 간격 추가
-        AnimatedDefaultTextStyle(
-          duration: const Duration(milliseconds: 50),
+        Text(
+          widget.text,
           style: TextStyle(
-            color: !isDisabled
-                ? Pallete.sodamBrown
-                : Colors.grey, // 비활성화 시 텍스트 색상 변경
-            fontSize: isHovered ? 21 : 20,
+            color: isDisabled ? Colors.grey : Colors.black, // 비활성화 시 텍스트 색상 변경
+            fontSize: 20,
             fontFamily: "IBMPlexSansKRBold",
-          ),
-          child: Text(
-            widget.text,
           ),
         ),
       ],
