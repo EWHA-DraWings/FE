34a1ee9026ef462672d171636626c224a250b215
lrenais <bleuing@ewhain.net>
2024-09-12 00:09:32 +0900

    자가진단 결과 화면 수정(0912)

--- sodam/lib/widgets/title_widget.dart
+++ sodam/lib/widgets/title_widget.dart
@@ -29,7 +29,7 @@ class TitleWidget extends StatelessWidget {
             text,
             style: TextStyle(
               color: textColor,
-              fontSize: 25,
+              fontSize: 30,
               fontFamily: "IBMPlexSansKRBold",
             ),
           ),
