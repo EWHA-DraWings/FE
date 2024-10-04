6353370d3f2aba70176d6f972ee26c4883fc02a9
Xeoyeon <mariajang16@ewhain.net>
2024-10-04 12:55:51 +0900

    Memoryscore_detail_screen

--- sodam/lib/main.dart
+++ sodam/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:sodam/models/emotion_data.dart';
 import 'package:sodam/screens/chat/diary_chat_screen.dart';
 import 'package:sodam/screens/login_screen.dart';
+import 'package:sodam/screens/report/memoryscore_detail_screen.dart';
 import 'package:sodam/screens/signin_start_screen.dart';
 import 'package:sodam/screens/main_screen.dart';
 import 'package:sodam/screens/report/report_detail_screen.dart';
@@ -31,29 +32,30 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: //MainScreen(isGuardian: false),
           //MembershipNameScreen(data: GuardianData(role:"guardian"),),
           //UserDiagnosisScreen(),
           //ReportMainScreen(),
           //SigninStartScreen(),
-          LoginScreen(),
-      //ReportDetailScreen(),
-      //DiaryChatScreen(),
-      //PastReport(),
-      //TimeSelectScreen(),
-      //     ReportMainScreen(
-      //   name: '홍길동',
-      //   daysPast: 3,
-      //   emotions: [
-      //     EmotionData(emotion: '슬픔', percentage: 50.0),
-      //     EmotionData(emotion: '행복', percentage: 40.0),
-      //     EmotionData(emotion: '분노', percentage: 10.0),
-      //   ],
-      // ),
-      //ReportDetailScreen(),
-      //DiaryChatScreen(),
-      //PastReport(),
+          //LoginScreen(),
+          //ReportDetailScreen(),
+          //DiaryChatScreen(),
+          //PastReport(),
+          //TimeSelectScreen(),
+          //     ReportMainScreen(
+          //   name: '홍길동',
+          //   daysPast: 3,
+          //   emotions: [
+          //     EmotionData(emotion: '슬픔', percentage: 50.0),
+          //     EmotionData(emotion: '행복', percentage: 40.0),
+          //     EmotionData(emotion: '분노', percentage: 10.0),
+          //   ],
+          // ),
+          //ReportDetailScreen(),
+          //DiaryChatScreen(),
+          //PastReport(),
+          MemoryscoreDetailScreen(),
     );
   }
 }
