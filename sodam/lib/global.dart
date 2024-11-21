ab20c591a11fdc175d71e54babaac9a6f5f5c1e9
Xeoyeon <mariajang16@ewhain.net>
2024-11-21 17:42:21 +0900

    cherrypicking

--- sodam/lib/global.dart
+++ sodam/lib/global.dart
@@ -2,5 +2,5 @@
 class Global {
   //로컬 서버로 돌리는 법(ipconfig ipv4 주소 필요)
   //BE clone -> 필요한 패키지들 설치 -> 실행해서 서버 돌리기
-  static const ipAddr = '43.201.114.235';
+  static const ipAddr = '15.164.217.147';
 }
